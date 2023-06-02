175f4476efbcf4924fdb4dc7c1ff0bbf4108ff2d
Darshan Babu <darshan@tereotech.com>
2023-06-02 18:07:16 +0530

    redirect

--- resources/views/payment_success.blade.php
+++ resources/views/payment_success.blade.php
@@ -36,6 +36,7 @@
       }
     </style>
     @endsection
+    <meta http-equiv = "refresh" content = "5; url = {{ route('home') }}" />
     <div class="card">
       <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
         <i class="checkmark">✓</i>
