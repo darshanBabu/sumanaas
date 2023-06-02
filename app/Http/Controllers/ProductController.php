ee15d90df7ca6312789d28b98746d1c4a6cd5c5c
Darshan Babu <darshan@tereotech.com>
2023-06-02 17:51:45 +0530

    removed client secret

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,15 +8,16 @@ use Auth;
 
 class ProductController extends Controller
 {
-    public function productList(){
+    public function productList()
+    {
         $data['products'] = Products::getAllProducts();
         return view('products_list', $data);
     }
 
-    public function productDetails(Request $request){
+    public function productDetails(Request $request)
+    {
         $data['product'] = Products::getProductDetails($request->id);
         $user = Auth::user();
-        $data['clientSecret'] = 'sk_test_51NE8YlSIS5dQ7W0Teb3z112mYJcyrK2rXy5bF1laBbw8FwvhEbNefUES7XcyzblH1p6jF2gpP3qUmevHqTcmfvFQ00uiHyai6o';
         $data['user'] = $user;
         $data['intent'] = $user->createSetupIntent();
         return view('product_details', $data);
